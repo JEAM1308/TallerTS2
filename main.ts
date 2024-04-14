3142169359b4c42957a7bc1614dc467c4bf6c540
Juan Arboleda <j.arboledam@uniandes.edu.co>
2024-04-14 16:02:27 -0500

    updates

--- main.ts
+++ main.ts
@@ -5,6 +5,7 @@ import { dataSeries } from './data.js';
 
 let seriesTbody: HTMLElement = document.getElementById('series')!;
 let avgSpan: HTMLElement = document.getElementById('seasons-avg')!;
+let row1: HTMLElement = document.getElementById('row1')!;
 
 function renderSeriesInTable(series: Serie[]): void {
   console.log('Cargando series...');
@@ -29,7 +30,53 @@ function getSeasonsAvg(series: Serie[]): number {
   return totalSeasons/quantElem;
 }
 
+function buscarSeriePorNombre(series: Serie[], nombre: string): Serie|undefined { 
+  for (const elemento of series) {
+    if (elemento.name === nombre) {
+      return elemento;
+    }
+  }
+}
+
+function seeDetails(serie: Serie): void { 
+  let cardDiv = document.createElement("div");
+  cardDiv.className = "col";
+  cardDiv.className = "card-container";
+  cardDiv.innerHTML = `<div class="card" style="width: 18rem;">
+                          <img class="card-img-top" src="${serie.image}" alt="Card image cap">
+                          <div class="card-body">
+                            <h5 class="card-title">${serie.name}</h5>
+                            <p class="card-text">${serie.description}</p>
+                            <a href="${serie.link}" class="btn btn-primary">Go to page</a>
+                          </div>
+                        </div>`;
+  if (row1 && row1.children.length >= 2) {
+  const segundoHijo: HTMLElement | null = row1.children[1] as HTMLElement | null;
+    if (segundoHijo) {
+      row1.removeChild(segundoHijo);
+    }
+  }
+  row1.appendChild(cardDiv);
+}
+
+
+
 renderSeriesInTable(dataSeries);
 
 avgSpan.innerHTML = `${getSeasonsAvg(dataSeries).toFixed(1)}`;
 
+let nameTag: HTMLCollectionOf<Element> = document.getElementsByClassName("nameTag");
+
+for (let i = 0; i < nameTag.length; i++) {
+  nameTag[i].addEventListener('click', () => {
+    clicEnCelda(nameTag[i] as HTMLElement);
+  });
+}
+
+function clicEnCelda(celda: HTMLElement) {
+  const contenido: string= celda.innerText;
+  let serie: Serie | undefined = buscarSeriePorNombre(dataSeries, contenido);
+  if (serie !== undefined ) {
+    seeDetails(serie);
+  }
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 import { dataSeries } from './data.js';
 var seriesTbody = document.getElementById('series');
 var avgSpan = document.getElementById('seasons-avg');
+var row1 = document.getElementById('row1');
 function renderSeriesInTable(series) {
     console.log('Cargando series...');
     series.forEach(function (serie) {
@@ -18,5 +19,42 @@ function getSeasonsAvg(series) {
     });
     return totalSeasons / quantElem;
 }
+function buscarSeriePorNombre(series, nombre) {
+    for (var _i = 0, series_1 = series; _i < series_1.length; _i++) {
+        var elemento = series_1[_i];
+        if (elemento.name === nombre) {
+            return elemento;
+        }
+    }
+}
+function seeDetails(serie) {
+    var cardDiv = document.createElement("div");
+    cardDiv.className = "col";
+    cardDiv.className = "card-container";
+    cardDiv.innerHTML = "<div class=\"card\" style=\"width: 18rem;\">\n                          <img class=\"card-img-top\" src=\"".concat(serie.image, "\" alt=\"Card image cap\">\n                          <div class=\"card-body\">\n                            <h5 class=\"card-title\">").concat(serie.name, "</h5>\n                            <p class=\"card-text\">").concat(serie.description, "</p>\n                            <a href=\"").concat(serie.link, "\" class=\"btn btn-primary\">Go to page</a>\n                          </div>\n                        </div>");
+    if (row1 && row1.children.length >= 2) {
+        var segundoHijo = row1.children[1]; // Obtén el segundo hijo
+        if (segundoHijo) {
+            row1.removeChild(segundoHijo);
+        }
+    }
+    row1.appendChild(cardDiv);
+}
 renderSeriesInTable(dataSeries);
 avgSpan.innerHTML = "".concat(getSeasonsAvg(dataSeries).toFixed(1));
+var nameTag = document.getElementsByClassName("nameTag");
+var _loop_1 = function (i) {
+    nameTag[i].addEventListener('click', function () {
+        clicEnCelda(nameTag[i]);
+    });
+};
+for (var i = 0; i < nameTag.length; i++) {
+    _loop_1(i);
+}
+function clicEnCelda(celda) {
+    var contenido = celda.innerText;
+    var serie = buscarSeriePorNombre(dataSeries, contenido);
+    if (serie !== undefined) {
+        seeDetails(serie);
+    }
+}
